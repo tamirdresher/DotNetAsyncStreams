4153f97e5fe470c49bcd5abdaaa957092c3ae9d3
Tamir Dresher <tamirdr@payoneer.com>
2021-03-15 11:43:53 +0200

    IEnumerable and Iterator

---
+++ DotNetAsyncStreams/AsyncStreams/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AsyncStreams
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var records = ReadLines("transactions.csv");
+            
+            var grps = from r in records
+                where r.Length > 0
+                group r by r[0] into g
+                select g;
+
+            foreach (var grp in grps)
+            {
+                Console.WriteLine($"Key: {grp.Key} Count: {grp.Count()}");
+            }
+        }
+
+        public static IEnumerable<string[]> ReadLines(string path)
+        {
+            var lines = System.IO.File.ReadAllLines(path);
+            foreach (var line in lines)
+            {
+                yield return line.Split(',');
+            }
+        }
+    }
+}
